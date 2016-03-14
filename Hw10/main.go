b3b7c10f2b52026806f09cdf0838fd2ff760aec3
Carlos Daccarett <cdaccarett@mail.fresnostate.edu>
2016-03-14 13:20:13 -0700

    Hw 10 Done

---
+++ Hw10/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func reqUrl(res http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(res, "localhost:8080/name%v", req.URL.Path)
+}
+
+func main() {
+	http.HandleFunc("/", reqUrl)
+	http.ListenAndServe(":8080", nil)
+}
